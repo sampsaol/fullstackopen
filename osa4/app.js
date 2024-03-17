a2df3757dd4592488e8123ee92f20e54edaaa228
Sampsa Ollikainen <sampsa.ollikainen@gmail.com>
2024-03-17 17:37:59 +0200

    task 4.2

--- osa4/app.js
+++ osa4/app.js
@@ -2,19 +2,12 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const notesRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
-
-const blogSchema = mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -29,23 +22,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
+app.use('/api/blogs', notesRouter)
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
